0eb883f111bbf216b392831d55190a266d65a2c9
Jesse Lisser <jesselisser@gmail.com>
2019-08-11 00:43:50 +0200

    add hero and fix page layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,19 @@ import "../index.scss";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Hero from "../components/hero";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="container">
-      <h1>Hi people</h1>
-      <p>My name is Jesse Lisser.</p>
-      <p>I like shiny things.</p>
-      <Link to="/page-2/">Go to page 2</Link>
-    </div>
+      <Hero title="Home" subtitle="-"></Hero>
+      <section className="section">
+        <div className="container">
+          <p>Hi people</p>
+          <p>My name is Jesse Lisser.</p>
+          <p>I like shiny things.</p>
+        </div>
+      </section>
   </Layout>
 )
 
--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,18 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Hero from "../components/hero";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Recipes" />
-    <div className="container">
-      <h1>Hi people</h1>
-      <p>My name is Jesse Lisser.</p>
-      <p>I like shiny things.</p>
-      <Link to="/page-2/">Go to page 2</Link>
-    </div>
+      <Hero title="Recipes" subtitle="Yumm"></Hero>
+      <section className="section">
+        <div className="container">
+          <p>
+          Here will a nice recipe overview page.
+        </p>
+        </div>
+      </section>
+     
   </Layout>
 )
 
