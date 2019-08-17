e0fcefa416d7b8c82e33eb36ae1fd432ef91e416
Jesse Lisser <jesselisser@gmail.com>
2019-08-17 22:49:36 +0200

    link to recipe page

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -9,7 +9,10 @@ const IndexPage = () => {
   const data = useStaticQuery(
     graphql`
       query dataQuery {
-        allMarkdownRemark {
+        allMarkdownRemark(limit: 5, sort: {
+          order: DESC,
+          fields: [frontmatter___date]
+        }) {
           edges {
             node {
               excerpt
@@ -31,30 +34,32 @@ const IndexPage = () => {
       <section className="section">
         <div className="container">
           <div className="columns is-multiline">
-            {data.allMarkdownRemark.edges.map((edge, index) => (
-              <div className="column is-one-third" key={index}>
-                <div className="card">
-                  <div className="card-image">
-                    <figure className="image is-4by3">
-                      <img
-                        src="https://bulma.io/images/placeholders/1280x960.png"
-                        alt="Placeholder"
-                      ></img>
-                    </figure>
-                  </div>
-                  <div className="card-content">
-                    <div className="media">
-                      <div className="media-content">
-                        <p className="title is-4">
-                          {edge.node.frontmatter.title}
-                        </p>
+            {data.allMarkdownRemark.edges.map((edge) => (
+              <div className="column is-one-third" key={edge.node.frontmatter.slug}>
+                <Link to={`/recipes${edge.node.frontmatter.slug}`}>
+                  <div className="card">
+                    <div className="card-image">
+                      <figure className="image is-4by3">
+                        <img
+                          src="https://bulma.io/images/placeholders/1280x960.png"
+                          alt="Placeholder"
+                        ></img>
+                      </figure>
+                    </div>
+                    <div className="card-content">
+                      <div className="media">
+                        <div className="media-content">
+                          <p className="title is-4">
+                            {edge.node.frontmatter.title}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="content">
+                        {edge.node.excerpt}
                       </div>
                     </div>
-                    <div className="content">
-                      {edge.node.excerpt}
-                    </div>
                   </div>
-                </div>
+                </Link>
               </div>
             ))}
           </div>
