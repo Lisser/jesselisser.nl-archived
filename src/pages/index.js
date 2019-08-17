624e8a1b5e75dd07bde43a089c07d2e3ce69be27
Jesse Lisser <jesselisser@gmail.com>
2019-08-17 22:19:05 +0200

    improve insta-feed

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,39 +51,54 @@ const IndexPage = () => {
           </p>
         </div>
       </section>
-      <section className="section">
-        <div className="container">
-          <div className="columns is-multiline">
+      <div className="container">
+        <div className="card">
+
+          <header class="card-header">
+            
+            <p class="card-header-title">
+              <a className="u-url is-link" href="https://www.instagram.com/messy_.jesse" aria-label="Instagram" rel="me">
+                messy_.jesse
+              </a>
+            </p>
+
+            <a className="card-header-icon u-url" href="https://www.instagram.com/messy_.jesse" aria-label="Instagram" rel="me">
+              <span className="icon">
+                <i className="fab fa-instagram"></i>
+              </span>
+            </a>
+          </header>
+
+          <div className="columns is-multiline is-mobile is-tiny is-variable insta-columns">
             {data.allInstaNode.edges.map(edge => (
-              <div className="column is-one-third" key={edge.node.id}>
-                <div className="card ">
-                  {/* <header className="card-header">
-                    <p className="card-header-title">{formatDistance(new Date(edge.node.timestamp * 1000), new Date())} - {edge.node.username}</p>
-                    <div className="card-header-icon">
-                      <a href={`https://www.instagram.com/p/${edge.node.id}`}>
-                        <span className="card-header-icon icon">
-                          <i className="fab fa-instagram"></i>
-                        </span>
-                      </a>
-                    </div>
-                  </header> */}
-                  <a href={`https://www.instagram.com/p/${edge.node.id}`}>
-                    <div className="card-image">
-                      <figure className="image is-1by1">
-                        <img
-                          style={{ objectFit: 'cover' }}
-                          src={edge.node.localFile.childImageSharp.fixed.src}
-                          alt="Placeholder"
-                        ></img>
-                      </figure>
-                    </div>
-                  </a>
-                </div>
+              <div className="column is-one-third " key={edge.node.id}>
+                {/* <header className="card-header">
+                  <p className="card-header-title">{formatDistance(new Date(edge.node.timestamp * 1000), new Date())} - {edge.node.username}</p>
+                  <div className="card-header-icon">
+                    <a href={`https://www.instagram.com/p/${edge.node.id}`}>
+                      <span className="card-header-icon icon">
+                        <i className="fab fa-instagram"></i>
+                      </span>
+                    </a>
+                  </div>
+                </header> */}
+                <a href={`https://www.instagram.com/p/${edge.node.id}`}>
+                  <div className="card-image">
+                    <figure className="image is-1by1">
+                      <img
+                        style={{ objectFit: 'cover' }}
+                        src={edge.node.localFile.childImageSharp.fixed.src}
+                        alt="Placeholder"
+                      ></img>
+                    </figure>
+                  </div>
+                </a>
               </div>
             ))}
           </div>
+
         </div>
-      </section>
+      </div>
     </Layout>
   );
 };
