fc92f07a0633223cd20b1f335373bb1ddfb5814a
Jesse Lisser <jesselisser@gmail.com>
2019-09-08 16:18:36 +0200

    tokio background image

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,13 @@
 import React from "react"
 
 const Hero = ({ title, subtitle }) => (
-  <section className="hero background-img is-large" style={{backgroundImage: `url('')`}}>
+  <section id="hero" className="hero background-img is-large">
     <div className="hero-body">
       <div className="container has-text-centered">
         <h1 className="title has-text-white is-size-1 has-text-weight-bold">
           {title}
         </h1>
-        <h2 class="subtitle has-text-light">
+        <h2 className="subtitle has-text-light">
           {subtitle}
         </h2>
       </div>
